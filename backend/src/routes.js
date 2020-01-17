d1e25b355a7ab0ea5dcc8968574eb7e0f4ca0798
gtargino <gustavodiniztargino@hotmail.com>
2020-01-17 00:00:29 -0300

    chore: DB connection and routes.js creation

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,22 +1,21 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
-
+const { Router } = require('express');
+const routes = Router();
 
 // Param types //
 
 // Query params: req.query (to filter, order, pagination, etc.)
-// app.get('/users', (req,res) => {
+// routes.get('/users', (req,res) => {
 //     return res.json({message: `This is a query param request! By: ${ req.query.name }`});
 // });
 
 // Route params: req.params (to identify a resource on PUT, DELETE, etc.)
-// app.put('/users/:user', (req,res) => {
+// routes.put('/users/:user', (req,res) => {
 //     return res.json({message: `This is a query param request! By: ${ req.params.user }`});
 // });
 
 // Body: req.body.name (to create/change registry and more)
-app.post('/users/', (req,res) => {
+routes.post('/users/', (req,res) => {
     return res.json({message: `This is a request (with body)! By: ${ req.body.name }`});
 });
-app.listen(3333);
+
+module.exports = routes;
